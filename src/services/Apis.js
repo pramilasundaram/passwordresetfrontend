385de16ce45a79e6fed1d45bda9415d018c9e72b
pramila <pramilaworld@gmail.com>
2023-07-11 07:14:12 -0700

    final commit

--- src/services/Apis.js
+++ src/services/Apis.js
@@ -14,22 +14,3 @@ export const passwordresetfunction=async(data,id,token)=>{
     return await commonrequest('POST',`${BASE_URL}/auth/resetpassword/${id}/${token}`,data)
 }
 
-// export const usergetfunction=async(search,gender,sort,page)=>{
-//     return await commonrequest('GET',`${BASE_URL}/user/getallusers?search=${search}&gender=${gender}&sort=${sort}&page=${page}`,"")    
-// }
-
-// export const deletefunction=async(id)=>{
-//     return await commonrequest('DELETE',`${BASE_URL}/user/delete/${id}`,{})    
-// }
-
-// export const getsingleuserfunction=async(id)=>{
-//     return await commonrequest('GET',`${BASE_URL}/user/getuser/${id}`,"")
-// }
-
-// export const editfunction=async(id,data,header)=>{
-//     return await commonrequest('PUT',`${BASE_URL}/user/edit/${id}`,data,header)
-// }
-
-// export const exporttocsvfunction=async()=>{
-//     return await commonrequest('GET',`${BASE_URL}/user/userexport`,"")
-// }
